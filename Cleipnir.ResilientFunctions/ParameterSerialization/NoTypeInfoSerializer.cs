614b941edef4faffd3137ec63353826024010d83
stidsborg <stidsborg@cleipnir.net>
2022-01-16 10:33:38 +0100

    Added NoTypeInfoSerializer and -Deserializer

---
+++ Cleipnir.ResilientFunctions/ParameterSerialization/NoTypeInfoSerializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace Cleipnir.ResilientFunctions.ParameterSerialization;
+
+public class NoTypeInfoSerializer : ISerializer, IDeserializer
+{
+    public static NoTypeInfoSerializer Instance { get; } = new();
+    
+    public string Serialize(object parameter, ParameterType parameterType, RFuncType rFuncType) 
+        => JsonConvert.SerializeObject(parameter);
+
+    public object DeserializeParameter(
+        string json,
+        string storedParameterType,
+        ParameterType parameterType,
+        RFuncType rFuncType
+    ) => parameterType switch
+    {
+        ParameterType.InputParameter => JsonConvert.DeserializeObject(json, rFuncType.ParameterType),
+        ParameterType.Scrapbook => JsonConvert.DeserializeObject(json, rFuncType.ScrapbookType!),
+        ParameterType.ReturnValue => JsonConvert.DeserializeObject(json, rFuncType.ReturnValueType!),
+        _ => throw new ArgumentOutOfRangeException(nameof(parameterType), parameterType, null)
+    } ?? throw new SerializationException($"Parameter '{parameterType}' was deserialized to null");
+}
