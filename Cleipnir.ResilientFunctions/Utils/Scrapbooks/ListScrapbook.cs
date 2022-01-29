2688e609431d8a9272c982d31b87409e7ee5eb13
stidsborg <stidsborg@cleipnir.net>
2022-01-29 10:45:14 +0100

    Added general-purpose ListScrapbook utility class

---
+++ Cleipnir.ResilientFunctions/Utils/Scrapbooks/ListScrapbook.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using Cleipnir.ResilientFunctions.Domain;
+
+namespace Cleipnir.ResilientFunctions.Utils.Scrapbooks;
+
+public class ListScrapbook<T> : RScrapbook
+{
+    public List<T> List { get; set; } = new();
+}
