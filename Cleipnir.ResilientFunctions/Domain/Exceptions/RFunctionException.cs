643365ca58294874e9cd4f6ead4a600c01e1a43a
stidsborg <stidsborg@cleipnir.net>
2022-03-02 12:19:39 +0100

    Simplified Framework- and RFunctionException

--- Cleipnir.ResilientFunctions/Domain/Exceptions/RFunctionException.cs
+++ Cleipnir.ResilientFunctions/Domain/Exceptions/RFunctionException.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Runtime.Serialization;
 
 namespace Cleipnir.ResilientFunctions.Domain.Exceptions;
 
 public abstract class RFunctionException : Exception
 {
-    public RFunctionException() { }
-
-    public RFunctionException(SerializationInfo info, StreamingContext context) : base(info, context) { }
-
     public RFunctionException(string? message) : base(message) { }
 
     public RFunctionException(string? message, Exception? innerException) : base(message, innerException) { }
