227b570d82824190c5956eccc4b55254e84bb1d1
stidsborg <stidsborg@cleipnir.net>
2022-03-06 09:57:27 +0100

    Align SimpleHttpAndDbExample to documentation

--- Samples/Sample.ConsoleApp/Simple/SimpleHttpAndDbExample.cs
+++ Samples/Sample.ConsoleApp/Simple/SimpleHttpAndDbExample.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Net.Http;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Cleipnir.ResilientFunctions;
 using Cleipnir.ResilientFunctions.Domain;
 using Cleipnir.ResilientFunctions.Helpers;
 using Cleipnir.ResilientFunctions.Storage;
-using Cleipnir.ResilientFunctions.Utils.Scrapbooks;
 using Dapper;
 
 namespace ConsoleApp.Simple;
@@ -17,61 +14,25 @@ public class SimpleHttpAndDbExample
 {
     private const string URL = "https://postman-echo.com/post";
     
-    public static async Task Perform(IFunctionStore store, IDbConnection connection)
+    public static async Task Perform(IDbConnection connection)
     {
+        var store = new InMemoryFunctionStore();
         var functions = new RFunctions(store, unhandledExceptionHandler: Console.WriteLine);
         var httpClient = new HttpClient();
 
-        var rFunc = functions.Register(
+        var rAction = functions.Register(
             "SimpleSaga",
             async (Guid id) =>
             {
                 var response = await httpClient.PostAsync(URL, new StringContent(id.ToString()));
                 response.EnsureSuccessStatusCode();
                 var content = await response.Content.ReadAsStringAsync();
-                await connection.ExecuteAsync("UPDATE Entity SET State=@State", new {State = content});
+                await connection.ExecuteAsync("UPDATE Entity SET State=@State WHERE Id=@Id", new {State = content, Id = id});
                 return Return.Succeed;
             }
         ).Invoke;
 
         var id = Guid.NewGuid();
-        await rFunc(id.ToString(), id).EnsureSuccess();
-    }
-    
-    public static async Task DoWithScrapbook()
-    {
-        var store = new InMemoryFunctionStore();
-        var rFunctions = new RFunctions(store, unhandledExceptionHandler: Console.WriteLine);
-        
-        var httpClient = new HttpClient();
-
-        var rFunc = rFunctions.Register<string, ListScrapbook<string>, List<string>>(
-            "pair-of-http-calls".ToFunctionTypeId(),
-            async (s, scrapbook) =>
-            {
-                for (var i = scrapbook.List.Count; i < 2; i++)
-                {
-                    var response = await httpClient
-                        .PostAsync(
-                            "https://postman-echo.com/post",
-                            new StringContent(s)
-                        );
-                    response.EnsureSuccessStatusCode();
-                    var content = await response.Content.ReadAsStringAsync();
-                    var echo = Regex
-                        .Match(
-                            content, 
-                            @"""data"":""(?<echo>[\w\s]+)",  
-                            RegexOptions.IgnoreCase
-                        ).Groups["echo"].ToString();
-                    scrapbook.List.Add(echo);
-                    await scrapbook.Save();
-                }
-                return scrapbook.List;
-            }
-        ).Invoke;
-
-        var response = await rFunc("hello resilient world!", "hello resilient world!").EnsureSuccess();
-        Console.WriteLine(string.Join(Environment.NewLine, response));
+        await rAction(id.ToString(), id).EnsureSuccess();
     }
 }
