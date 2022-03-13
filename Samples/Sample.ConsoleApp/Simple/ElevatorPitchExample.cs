57b17f86a2ad375967a864fe4002844a8d8f72d0
stidsborg <stidsborg@cleipnir.net>
2022-03-13 11:17:32 +0100

    Added ElevatorPitchExample

---
+++ Samples/Sample.ConsoleApp/Simple/ElevatorPitchExample.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Cleipnir.ResilientFunctions;
+using Cleipnir.ResilientFunctions.Domain;
+using Cleipnir.ResilientFunctions.SqlServer;
+using Serilog;
+
+namespace ConsoleApp.Simple;
+
+public static class ElevatorPitchExample
+{
+    private static readonly HttpClient HttpClient = new HttpClient();
+    
+    public static async Task ElevatorPitch(string connectionString)
+    {
+        var store = new SqlServerFunctionStore(connectionString); //simple to use SqlServer as function storage layer - other stores also exist!
+        await store.Initialize(); //create table in database - btw the invocation is idempotent!
+
+        var rFunctions = new RFunctions( //this is where you register different resilient function types
+            store,
+            unhandledExceptionHandler: //framework exceptions are simply to log and handle otherwise - just register a handler
+                e => Log.Error(e, "Resilient Function Framework exception occured"),
+            crashedCheckFrequency: TimeSpan.FromMinutes(1), // you have the control in deciding the sweet spot 
+            postponedCheckFrequency: TimeSpan.FromMinutes(1) // between quick reaction and pressure on the function store
+        );
+
+        var registration = rFunctions.Register( //making a function resilient is simply a matter of registering it
+            functionTypeId: "HttpGetSaga", //a specific resilient function is identified by type and instance id - instance id is provided on invocation
+            inner: async Task<Return<string>>(string url) => await HttpClient.GetStringAsync(url) //this is the function you are making resilient!
+        ); //btw no need to define a cluster - just register it on multiple nodes to get redundancy!
+           //and any crashed invocation of the function type will automatically be picked after this point
+
+        var rFunc = registration.Invoke; //you can also re-invoke (useful for manual handling) an existing function or schedule one for invocation
+        const string url = "https://google.com";
+        var result = await rFunc(functionInstanceId: "google", param: url); //invoking the function - btw you can F11-debug from here into your registered function
+        var responseBody = result.EnsureSuccess(); // you can also check if the invocation failed or was postponed
+        Log.Information("Resilient Function getting {Url} completed successfully with {Body}", url, responseBody);
+        
+        await rFunctions.ShutdownGracefully(); //waits for currently invoking functions to complete before shutdown - otw just do not await!
+    }
+}
+
