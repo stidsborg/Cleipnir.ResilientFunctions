7bc0c3783eab4cd945b17bee3a095c29abdcedba
stidsborg <stidsborg@cleipnir.net>
2022-04-15 10:30:35 +0200

    Fixed concurrency bug in ActionDisposable

--- Cleipnir.ResilientFunctions/Helpers/Disposables/ActionDisposable.cs
+++ Cleipnir.ResilientFunctions/Helpers/Disposables/ActionDisposable.cs
@@ -6,13 +6,18 @@ public class ActionDisposable : IDisposable
 {
     private readonly Action _dispose;
     private bool _disposed;
+    private readonly object _sync = new();
 
     public ActionDisposable(Action dispose) => _dispose = dispose;
 
     public void Dispose()
     {
-        if (_disposed) return;
-        _disposed = true;
+        lock (_sync)
+        {
+            if (_disposed) return;
+            _disposed = true;
+        }
+        
         _dispose();
     }
 }
