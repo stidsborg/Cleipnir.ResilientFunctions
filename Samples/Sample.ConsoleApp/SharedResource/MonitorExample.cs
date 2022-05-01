b3fa8ae2c8376380e612ec15a2230fdba14a5778
stidsborg <stidsborg@cleipnir.net>
2022-05-01 11:22:39 +0200

    Enabled nullable in Sample.ConsoleApp project

--- Samples/Sample.ConsoleApp/SharedResource/MonitorExample.cs
+++ Samples/Sample.ConsoleApp/SharedResource/MonitorExample.cs
@@ -10,6 +10,13 @@ public class MonitorExample
     private ISharedResourceApi1 ResourceApi1 { get; }
     private ISharedResourceApi2 ResourceApi2 { get; }
 
+    public MonitorExample(IMonitor monitor, ISharedResourceApi1 resourceApi1, ISharedResourceApi2 resourceApi2)
+    {
+        Monitor = monitor;
+        ResourceApi1 = resourceApi1;
+        ResourceApi2 = resourceApi2;
+    }
+
     public async Task<Result> UpdateSubscription(string rFuncId, string resourceId, string value)
     {
         await using var @lock = await Monitor.Acquire(lockId: nameof(UpdateSubscription), keyId: rFuncId);
