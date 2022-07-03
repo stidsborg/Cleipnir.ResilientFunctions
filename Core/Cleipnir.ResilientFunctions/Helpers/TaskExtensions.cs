b273e19b0183ab7bbfb5d88922d616838efd68da
stidsborg <stidsborg@cleipnir.net>
2022-07-03 09:32:54 +0200

    Added Pure-attribute to TaskExtensions

--- Core/Cleipnir.ResilientFunctions/Helpers/TaskExtensions.cs
+++ Core/Cleipnir.ResilientFunctions/Helpers/TaskExtensions.cs
@@ -1,14 +1,19 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Threading.Tasks;
 
 namespace Cleipnir.ResilientFunctions.Helpers;
 
 public static class TaskExtensions
 {
+    [Pure]
     public static async Task<List<T>> ToTaskAsync<T>(this Task<IEnumerable<T>> tasks) => new List<T>(await tasks);
+    [Pure]
     public static async Task<TOut> SelectAsync<TIn, TOut>(this Task<TIn> task, Func<TIn, TOut> selector)
         => selector(await task);
+    [Pure]
     public static Task<T> ToTask<T>(this T t) => Task.FromResult(t);
+    [Pure]
     public static T? ToNullable<T>(this T t) => (T?) t;
 }
