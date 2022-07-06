bc166354719176b047dcfdbac28389ad9835dd0b
stidsborg <stidsborg@cleipnir.net>
2022-07-06 11:59:12 +0200

    Introduced AsyncSemaphore

---
+++ Core/Cleipnir.ResilientFunctions.Tests/InMemoryTests/AsyncSemaphoreTests.cs
@@ -0,0 +1,80 @@
+﻿using System.Threading.Tasks;
+using Cleipnir.ResilientFunctions.Helpers;
+using Cleipnir.ResilientFunctions.Tests.Utils;
+using Cleipnir.ResilientFunctions.Watchdogs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace Cleipnir.ResilientFunctions.Tests.InMemoryTests;
+
+[TestClass]
+public class AsyncSemaphoreTests
+{
+    [TestMethod]
+    public async Task SemaphoreWithCapacityAllowsTakeImmediately()
+    {
+        var semaphore = new AsyncSemaphore(maxParallelism: 1);
+        var flag = new SyncedFlag();
+
+        {
+            using var @lock = await semaphore.Take();
+            flag.Raise();
+        }
+        
+        flag.IsRaised.ShouldBeTrue();
+    }
+    
+    [TestMethod]
+    public async Task EnqueuedWorkIsOnlyExecutedAfterPreviousWorkHasCompleted()
+    {
+        var semaphore = new AsyncSemaphore(maxParallelism: 1);
+        var m1Flag = new SyncedFlag();
+        var m2Flag = new SyncedFlag();
+
+        async Task M1()
+        {
+            using var @lock = await semaphore.Take();
+            await Task.Delay(100);
+            if (!m2Flag.IsRaised)
+                m1Flag.Raise();
+        }
+
+        async Task M2()
+        {
+            using var @lock = await semaphore.Take();
+            m2Flag.Raise();
+        }
+
+        _ = M1();
+        _ = M2();
+
+        await BusyWait.UntilAsync(() => m1Flag.IsRaised && m2Flag.IsRaised);
+    }
+    
+    [TestMethod]
+    public async Task WorkCanBeExecutedInParallel()
+    {
+        var semaphore = new AsyncSemaphore(maxParallelism: 2);
+        var m1Flag = new SyncedFlag();
+        var m2Flag = new SyncedFlag();
+
+        async Task M1()
+        {
+            using var @lock = await semaphore.Take();
+            await Task.Delay(100);
+            if (m2Flag.IsRaised)
+                m1Flag.Raise();
+        }
+
+        async Task M2()
+        {
+            using var @lock = await semaphore.Take();
+            m2Flag.Raise();
+        }
+
+        _ = M1();
+        _ = M2();
+
+        await BusyWait.UntilAsync(() => m1Flag.IsRaised && m2Flag.IsRaised);
+    }
+}
---
+++ Core/Cleipnir.ResilientFunctions/Watchdogs/AsyncSemaphore.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Cleipnir.ResilientFunctions.Watchdogs;
+
+public class AsyncSemaphore
+{
+    private int _freeWorkers;
+    private readonly Queue<TcsAndLock> _queue = new();
+    private readonly object _sync = new();
+
+    public AsyncSemaphore(int maxParallelism) => _freeWorkers = maxParallelism;
+
+    public Task<IDisposable> Take()
+    {
+        var tcs = new TaskCompletionSource<IDisposable>();
+        var @lock = new Lock();
+        var tcsAndLock = new TcsAndLock(tcs, @lock);
+        
+        lock (_sync)
+        {
+            if (_freeWorkers == 0)
+            {
+                _queue.Enqueue(tcsAndLock);
+                return tcs.Task;
+            }
+
+            _freeWorkers--;
+        }
+
+        _ = ExecuteWorker(tcsAndLock);
+        return tcs.Task;
+    }
+
+    private async Task ExecuteWorker(TcsAndLock? tcsAndLock)
+    {
+        if (tcsAndLock != null)
+        {
+            var (tcs, @lock) = tcsAndLock;
+            
+            tcs.TrySetResult(@lock);
+            await @lock.Disposed.Task;
+        }
+        
+        while (true)
+        {
+            TaskCompletionSource<IDisposable> tcs;
+            Lock @lock;
+            
+            lock (_sync)
+            {
+                if (_queue.Count == 0)
+                {
+                    _freeWorkers++;
+                    return;
+                }
+                
+                tcsAndLock = _queue.Dequeue();
+                tcs = tcsAndLock.Tcs;
+                @lock = tcsAndLock.Lock;
+            }
+            
+            tcs.TrySetResult(@lock);
+            await @lock.Disposed.Task;
+        }
+    }
+
+    private class Lock : IDisposable
+    {
+        public TaskCompletionSource Disposed { get;  } = new();
+
+        public void Dispose() => Disposed.TrySetResult();
+    }
+
+    private record TcsAndLock(TaskCompletionSource<IDisposable> Tcs, Lock Lock);
+}
