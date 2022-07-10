64e37e43ca852f3378099a4c164169fa9e03bc93
stidsborg <stidsborg@cleipnir.net>
2022-07-10 11:23:56 +0200

    Revert "Increasing CrashedCheckFrequency in CrashedTests"
    This reverts commit d473e694a2239f2306d132d129ea7d29f382698f.

--- Core/Cleipnir.ResilientFunctions.Tests/TestTemplates/RFunctionTests/CrashedTests.cs
+++ Core/Cleipnir.ResilientFunctions.Tests/TestTemplates/RFunctionTests/CrashedTests.cs
@@ -95,7 +95,7 @@ public abstract class CrashedTests
                 store,
                 new Settings(
                     unhandledExceptionHandler.Catch,
-                    CrashedCheckFrequency: TimeSpan.FromMilliseconds(100)
+                    CrashedCheckFrequency: TimeSpan.FromMilliseconds(2)
                 )
             );
 
@@ -159,7 +159,7 @@ public abstract class CrashedTests
                 store,
                 new Settings(
                     unhandledExceptionHandler.Catch,
-                    CrashedCheckFrequency: TimeSpan.FromMilliseconds(100)
+                    CrashedCheckFrequency: TimeSpan.FromMilliseconds(2)
                 )
             );
 
@@ -215,7 +215,7 @@ public abstract class CrashedTests
                 store,
                 new Settings(
                     unhandledExceptionHandler.Catch,
-                    CrashedCheckFrequency: TimeSpan.FromMilliseconds(100)
+                    CrashedCheckFrequency: TimeSpan.FromMilliseconds(2)
                 )
             );
 
