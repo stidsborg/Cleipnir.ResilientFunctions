14dfed7ecf5b5d4fa442b7ae071990dfd14177f1
stidsborg <stidsborg@cleipnir.net>
2022-07-13 14:52:36 +0200

    Implemented and using new stored events bulk mechanism in PostgreSqlEventStore

--- Messaging/Stores/PostgreSQL/Cleipnir.ResilientFunctions.Messaging.PostgreSQL/PostgreSqlEventStore.cs
+++ Messaging/Stores/PostgreSQL/Cleipnir.ResilientFunctions.Messaging.PostgreSQL/PostgreSqlEventStore.cs
@@ -30,9 +30,7 @@ public class PostgreSqlEventStore : IEventStore
                 function_type_id VARCHAR(255),
                 function_instance_id VARCHAR(255),
                 position INT NOT NULL,
-                event_json TEXT NOT NULL,
-                event_type VARCHAR(255) NOT NULL,   
-                idempotency_key VARCHAR(255),          
+                events_json TEXT NOT NULL,
                 PRIMARY KEY (function_type_id, function_instance_id, position)
             );";
         var command = new NpgsqlCommand(sql, conn);
@@ -55,31 +53,8 @@ public class PostgreSqlEventStore : IEventStore
         await command.ExecuteNonQueryAsync();
     }
 
-    public async Task AppendEvent(FunctionId functionId, StoredEvent storedEvent)
-    {
-        await using var conn = await CreateConnection();
-        var transaction = await conn.BeginTransactionAsync();
-        var (eventJson, eventType, idempotencyKey) = storedEvent;
-
-        var sql = @$"    
-                INSERT INTO {_tablePrefix}events
-                    (function_type_id, function_instance_id, position, event_json, event_type, idempotency_key)
-                VALUES
-                    ($1, $2, (SELECT COUNT(*) FROM {_tablePrefix}events WHERE function_type_id = $1 AND function_instance_id = $2), $3, $4, $5);";
-        await using var command = new NpgsqlCommand(sql, conn, transaction)
-        {
-            Parameters =
-            {
-                new() {Value = functionId.TypeId.Value},
-                new() {Value = functionId.InstanceId.Value},
-                new() {Value = eventJson},
-                new() {Value = eventType},
-                new() {Value = idempotencyKey ?? (object) DBNull.Value}
-            }
-        };
-        await command.ExecuteNonQueryAsync();
-        await transaction.CommitAsync();
-    }
+    public Task AppendEvent(FunctionId functionId, StoredEvent storedEvent)
+        => AppendEvents(functionId, storedEvents: new[] { storedEvent });
 
     public Task AppendEvent(FunctionId functionId, string eventJson, string eventType, string? idempotencyKey = null)
         => AppendEvent(functionId, new StoredEvent(eventJson, eventType, idempotencyKey));
@@ -87,37 +62,30 @@ public class PostgreSqlEventStore : IEventStore
     public async Task AppendEvents(FunctionId functionId, IEnumerable<StoredEvent> storedEvents)
     {
         await using var conn = await CreateConnection();
-        var transaction =  await conn.BeginTransactionAsync();
+        var json = StoredEventsBulkJsonSerializer.SerializeToJson(storedEvents);
 
-        foreach (var (eventJson, eventType, idempotencyKey) in storedEvents)
+        var sql = @$"    
+            INSERT INTO {_tablePrefix}events
+                (function_type_id, function_instance_id, position, events_json)
+            VALUES
+                ($1, $2, (SELECT COUNT(*) FROM {_tablePrefix}events WHERE function_type_id = $1 AND function_instance_id = $2), $3);";
+        await using var command = new NpgsqlCommand(sql, conn)
         {
-            var sql = @$"    
-                INSERT INTO {_tablePrefix}events
-                    (function_type_id, function_instance_id, position, event_json, event_type, idempotency_key)
-                VALUES
-                    ($1, $2, (SELECT COUNT(*) FROM {_tablePrefix}events WHERE function_type_id = $1 AND function_instance_id = $2), $3, $4, $5);";
-            await using var command = new NpgsqlCommand(sql, conn, transaction)
+            Parameters =
             {
-                Parameters =
-                {
-                    new() {Value = functionId.TypeId.Value},
-                    new() {Value = functionId.InstanceId.Value},
-                    new() {Value = eventJson},
-                    new() {Value = eventType},
-                    new() {Value = idempotencyKey ?? (object) DBNull.Value}
-                }
-            };
-            await command.ExecuteNonQueryAsync();
-        }
-        
-        await transaction.CommitAsync();
+                new() {Value = functionId.TypeId.Value},
+                new() {Value = functionId.InstanceId.Value},
+                new() {Value = json}
+            }
+        };
+        await command.ExecuteNonQueryAsync();
     }
 
     public async Task<IEnumerable<StoredEvent>> GetEvents(FunctionId functionId, int skip)
     {
         await using var conn = await CreateConnection();
         var sql = @$"    
-            SELECT event_json, event_type, idempotency_key
+            SELECT events_json
             FROM {_tablePrefix}events
             WHERE function_type_id = $1 AND function_instance_id = $2 AND position >= $3
             ORDER BY position ASC;";
@@ -135,10 +103,9 @@ public class PostgreSqlEventStore : IEventStore
         await using var reader = await command.ExecuteReaderAsync();
         while (await reader.ReadAsync())
         {
-            var eventJson = reader.GetString(0);
-            var messageJson = reader.GetString(1);
-            var idempotencyKey = reader.IsDBNull(2) ? null : reader.GetString(2);
-            storedEvents.Add(new StoredEvent(eventJson, messageJson, idempotencyKey));
+            var eventsJson = reader.GetString(0);
+            var deserializedStoredEvents = StoredEventsBulkJsonSerializer.DeserializeToStoredEvents(eventsJson);
+            storedEvents.AddRange(deserializedStoredEvents);
         }
 
         return storedEvents;
