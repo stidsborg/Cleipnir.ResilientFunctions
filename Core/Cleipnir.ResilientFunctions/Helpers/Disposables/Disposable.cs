b050f790e6248dde8545076c659dbc74c6d1469e
stidsborg <stidsborg@cleipnir.net>
2022-08-24 14:54:04 +0200

    Implemented NoOp-disposable type

--- Core/Cleipnir.ResilientFunctions/Helpers/Disposables/Disposable.cs
+++ Core/Cleipnir.ResilientFunctions/Helpers/Disposables/Disposable.cs
@@ -5,4 +5,5 @@ namespace Cleipnir.ResilientFunctions.Helpers.Disposables;
 public static class Disposable
 {
     public static IDisposable Combine(params IDisposable[] disposables) => new CombinedDisposables(disposables);
+    public static IDisposable NoOp() => NoOpDisposable.Instance;
 }
---
+++ Core/Cleipnir.ResilientFunctions/Helpers/Disposables/NoOpDisposable.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Cleipnir.ResilientFunctions.Helpers.Disposables;
+
+public class NoOpDisposable : IDisposable
+{
+    public static IDisposable Instance { get; } = new NoOpDisposable();
+    public void Dispose() { }
+}
