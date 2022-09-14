10cc741481c8a848ba328c6c5a949f587e134251
stidsborg <stidsborg@cleipnir.net>
2022-09-14 12:51:22 +0200

    Removed unused middleware related code

--- Core/Cleipnir.ResilientFunctions/Invocation/IMiddleware.cs
+++ Core/Cleipnir.ResilientFunctions/Invocation/IMiddleware.cs
@@ -6,25 +6,13 @@ namespace Cleipnir.ResilientFunctions.Invocation;
 
 public interface IMiddleware
 {
-    Task<Result> InvokeAction<TParam>(
-        TParam param,
-        Context context,
-        Func<TParam, Context, Task<Result>> next
-    ) where TParam : notnull;
-        
     Task<Result> InvokeAction<TParam, TScrapbook>(
         TParam param,
         TScrapbook scrapbook,
         Context context,
         Func<TParam, TScrapbook, Context, Task<Result>> next 
     ) where TParam : notnull where TScrapbook : RScrapbook, new();
-        
-    Task<Result<TResult>> InvokeFunc<TParam, TResult>(
-        TParam param,
-        Context context,
-        Func<TParam, Context, Task<Result<TResult>>> next 
-    ) where TParam : notnull;
-        
+
     Task<Result<TResult>> InvokeFunc<TParam, TScrapbook, TResult>(
         TParam param,
         TScrapbook scrapbook,
--- Core/Cleipnir.ResilientFunctions/Invocation/MiddlewarePipeline.cs
+++ Core/Cleipnir.ResilientFunctions/Invocation/MiddlewarePipeline.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Threading.Tasks;
 using Cleipnir.ResilientFunctions.Domain;
 
@@ -11,31 +10,6 @@ public class MiddlewarePipeline
     private readonly IReadOnlyList<MiddlewareOrResolver> _middlewares;
     
     public MiddlewarePipeline(IReadOnlyList<MiddlewareOrResolver> middlewares) => _middlewares = middlewares;
-
-    public Func<TParam, Context, Task<Result>> WrapPipelineAroundInnerAction<TParam>(
-        Func<TParam, Context, Task<Result>> inner, 
-        IScopedDependencyResolver? scopedDependencyResolver
-    ) where TParam : notnull
-    {
-        var curr = inner;
-        for (var i = _middlewares.Count - 1; i >= 0; i--)
-        {
-            var (middleware, resolver) = _middlewares[i];
-            if (resolver != null)
-                middleware = resolver(scopedDependencyResolver!);
-            var middlewareAction = new MiddlewareAction<TParam>(curr, middleware!);    
-            curr = middlewareAction.Invoke;
-        }
-
-        return curr;
-    }
-
-    private record MiddlewareAction<TParam> (Func<TParam, Context, Task<Result>> Next, IMiddleware Middleware) 
-        where TParam : notnull 
-    {
-        [DebuggerStepThrough]
-        public Task<Result> Invoke(TParam param, Context context) => Middleware.InvokeAction(param, context, Next);
-    }
     
     public Func<TParam, TScrapbook, Context, Task<Result>> WrapPipelineAroundInnerAction<TParam, TScrapbook>(
         Func<TParam, TScrapbook, Context, Task<Result>> inner, 
@@ -55,26 +29,7 @@ public class MiddlewarePipeline
 
         return curr;
     }
-    
-    public Func<TParam, Context, Task<Result<TReturn>>> WrapPipelineAroundInnerFunc<TParam, TReturn>(
-        Func<TParam, Context, Task<Result<TReturn>>> inner, 
-        IScopedDependencyResolver? scopedDependencyResolver
-    ) where TParam : notnull
-    {
-        var curr = inner;
-        for (var i = _middlewares.Count - 1; i >= 0; i--)
-        {
-            var next = curr;
-            var (middleware, resolver) = _middlewares[i];
-            if (resolver != null)
-                middleware = resolver(scopedDependencyResolver!);
-                
-            curr = (param, context) => middleware!.InvokeFunc(param, context, next);
-        }
 
-        return curr;
-    }
-    
     public Func<TParam, TScrapbook, Context, Task<Result<TReturn>>> WrapPipelineAroundInnerFunc<TParam, TScrapbook, TReturn>(
         Func<TParam, TScrapbook, Context, Task<Result<TReturn>>> inner, 
         IScopedDependencyResolver? scopedDependencyResolver
