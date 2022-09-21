ea12c9410a353ce033be0833df0bd1a317d98404
stidsborg <stidsborg@cleipnir.net>
2022-09-21 10:55:57 +0200

    Moved FunctionId related types into their own files

---
+++ Core/Cleipnir.ResilientFunctions/Domain/FunctionId.cs
@@ -0,0 +1,23 @@
+﻿namespace Cleipnir.ResilientFunctions.Domain;
+
+public record FunctionId(FunctionTypeId TypeId, FunctionInstanceId InstanceId)
+{
+    public FunctionId(string functionTypeId, string functionInstanceId) 
+        : this(functionTypeId.ToFunctionTypeId(), functionInstanceId.ToFunctionInstanceId()) { }
+        
+    public FunctionId(FunctionTypeId functionTypeId, string functionInstanceId) 
+        : this(functionTypeId, functionInstanceId.ToFunctionInstanceId()) { }
+
+    public FunctionId(string functionTypeId, FunctionInstanceId functionInstanceId) 
+        : this(functionTypeId.ToFunctionTypeId(), functionInstanceId) { }
+
+    public override string ToString() 
+        => $"{InstanceId}@{TypeId}";
+}
+
+public static class FunctionIdExtensions 
+{
+    public static FunctionTypeId ToFunctionTypeId(this string functionTypeId) => new(functionTypeId);
+
+    public static FunctionInstanceId ToFunctionInstanceId(this string functionInstanceId) => new(functionInstanceId);
+}
---
+++ Core/Cleipnir.ResilientFunctions/Domain/FunctionInstanceId.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Cleipnir.ResilientFunctions.Domain;
+
+public class FunctionInstanceId
+{
+    public string Value { get; }
+    public FunctionInstanceId(string value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        Value = value;
+    }
+    
+    public static implicit operator FunctionInstanceId(string functionInstanceId) => new(functionInstanceId);
+    public override string ToString() => Value;
+    public static bool operator ==(FunctionInstanceId id1, FunctionInstanceId id2) => id1.Equals(id2);
+    public static bool operator !=(FunctionInstanceId id1, FunctionInstanceId id2) => !(id1 == id2);
+
+    public override bool Equals(object? obj)
+        => obj is FunctionInstanceId id && id.Value == Value;
+    public override int GetHashCode() => Value.GetHashCode();
+}
