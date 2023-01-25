3ee5d496caac2baabe6aee48a6a3b6b90c211d6a
stidsborg <stidsborg@cleipnir.net>
2023-01-25 13:54:48 +0100

    Removed unused class

--- Core/Cleipnir.ResilientFunctions/Reactive/Subscription.cs
+++
@@ -1,5 +0,0 @@
-﻿using System;
-
-namespace Cleipnir.ResilientFunctions.Reactive;
-
-public record struct Subscription<T>(int SubscriptionId, Action<T> OnNext, Action OnCompletion, Action<Exception> OnException);
