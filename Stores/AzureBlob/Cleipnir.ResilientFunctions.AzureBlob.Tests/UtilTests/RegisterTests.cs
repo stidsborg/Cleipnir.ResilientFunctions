5882c3edce1b43fc65478653bb569e92dd0bd02b
stidsborg <stidsborg@cleipnir.net>
2023-05-10 13:56:56 +0200

    Completed the Utilities implementation for AzureBlob-storage

---
+++ Stores/AzureBlob/Cleipnir.ResilientFunctions.AzureBlob.Tests/UtilTests/RegisterTests.cs
@@ -0,0 +1,65 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Cleipnir.ResilientFunctions.Helpers;
+using Cleipnir.ResilientFunctions.Utils.Register;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cleipnir.ResilientFunctions.AzureBlob.Tests.UtilTests;
+
+[TestClass]
+public class RegisterTests : Cleipnir.ResilientFunctions.Tests.TestTemplates.UtilsTests.RegisterTests
+{
+    [TestMethod]
+    public override Task SetValueWithNoExistingValueSucceeds()
+        => SetValueWithNoExistingValueSucceeds(CreateAndInitializeRegister());
+
+    [TestMethod]
+    public override Task CompareAndSwapWithNoExistingValueSucceeds()
+        => CompareAndSwapWithNoExistingValueSucceeds(CreateAndInitializeRegister());
+
+    [TestMethod]
+    public override Task CompareAndSwapFailsWithNoExistingValue()
+        => CompareAndSwapFailsWithNoExistingValue(CreateAndInitializeRegister());
+
+    [TestMethod]
+    public override Task SetValueIfEmptyFailsWhenRegisterHasExistingValue()
+        => SetValueIfEmptyFailsWhenRegisterHasExistingValue(CreateAndInitializeRegister());
+
+    [TestMethod]
+    public override Task CompareAndSwapSucceedsIfAsExpected()
+        => CompareAndSwapSucceedsIfAsExpected(CreateAndInitializeRegister());
+
+    [TestMethod]
+    public override Task CompareAndSwapSucceedsIfAsExpectedIgnoreIfNoExisting()
+        => CompareAndSwapSucceedsIfAsExpectedIgnoreIfNoExisting(CreateAndInitializeRegister());
+
+    [TestMethod]
+    public override Task ExistsIfFalseForNonExistingRegister()
+        => ExistsIfFalseForNonExistingRegister(CreateAndInitializeRegister());
+
+    [TestMethod]
+    public override Task ExistingValueIsNullForNonExistingRegister()
+        => ExistingValueIsNullForNonExistingRegister(CreateAndInitializeRegister());
+
+    [TestMethod]
+    public override Task DeleteSucceedsForNonExistingRegister()
+        => DeleteSucceedsForNonExistingRegister(CreateAndInitializeRegister());
+
+    [TestMethod]
+    public override Task DeleteSucceedsForExistingRegister()
+        => DeleteSucceedsForExistingRegister(CreateAndInitializeRegister());
+
+    [TestMethod]
+    public override Task DeleteSucceedsWithExpectedValueForExistingRegister()
+        => DeleteSucceedsWithExpectedValueForExistingRegister(CreateAndInitializeRegister());
+
+    [TestMethod]
+    public override Task DeleteFailsWhenNonExpectedValueForExistingRegister()
+        => DeleteFailsWhenNonExpectedValueForExistingRegister(CreateAndInitializeRegister());
+
+    private Task<IRegister> CreateAndInitializeRegister([CallerMemberName] string memberName = "")
+    {
+        var underlyingRegister = new AzureBlobUnderlyingRegister(FunctionStoreFactory.BlobContainerClient);
+        return new Register(underlyingRegister).CastTo<IRegister>().ToTask();
+    }
+}
--- Stores/AzureBlob/Cleipnir.ResilientFunctions.AzureBlob/Utils.cs
+++ Stores/AzureBlob/Cleipnir.ResilientFunctions.AzureBlob/Utils.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text;
 using Cleipnir.ResilientFunctions.Domain;
+using Cleipnir.ResilientFunctions.Utils;
 
 namespace Cleipnir.ResilientFunctions.AzureBlob;
 
@@ -33,6 +34,25 @@ public static class Utils
     {
         functionId.Validate();
         return $"{functionId}_events";
+    }
+
+    internal static string GetUnderlyingRegisterName(RegisterType registerType, string group, string name)
+    {
+        return $"{(int) registerType}{group.Length:000}_{group}_{name}";
+    }
+
+    public record RegisterName(RegisterType RegisterType, string Group, string Name);
+    internal static RegisterName ConvertToRegisterName(string blobName)
+    {
+        var registerType = (RegisterType) int.Parse(blobName[0].ToString());
+        var rest = blobName[1..]; //skip register type
+        var groupLength = int.Parse(rest[..3]); 
+        rest = rest[4..]; //skip group length and _
+        var group = rest[..groupLength];
+        rest = rest[(groupLength + 1)..]; //skip group name and trailing _
+        var name = rest;
+
+        return new RegisterName(registerType, group, name);
     }
 
     internal static FunctionId ConvertFromStateBlobNameToFunctionId(this string blobName)
