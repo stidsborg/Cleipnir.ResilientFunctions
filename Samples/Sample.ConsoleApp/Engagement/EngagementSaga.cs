4537ec19273cc995c9d04bfe18596046f15f54f4
stidsborg <stidsborg@cleipnir.net>
2023-07-30 11:02:40 +0200

    Added EngagementSaga sample

---
+++ Samples/Sample.ConsoleApp/Engagement/EngagementSaga.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using Cleipnir.ResilientFunctions.CoreRuntime.Invocation;
+using Cleipnir.ResilientFunctions.Reactive;
+
+namespace ConsoleApp.Engagement;
+
+public static class EngagementSaga
+{
+    public static async Task Start(string candidateEmail, Context context)
+    {
+        var es = await context.EventSource;
+
+        await es.DoAtLeastOnce(
+            workId: "InitialCorrespondence",
+            SendEngagementInitialCorrespondence
+        );
+        
+        for (var i = 0; i < 10; i++)
+        {
+            var either = await es
+                .OfTypes<EngagementAccepted, EngagementRejected>()
+                .Where(either =>
+                    either.Match(
+                        first: a => a.Iteration == i,
+                        second: r => r.Iteration == i
+                    )
+                )
+                .SuspendUntilNext(timeoutEventId: i.ToString(), expiresIn: TimeSpan.FromHours(1));
+
+            var flowCompleted = await either.Match(
+                first: async a =>
+                {
+                    await es.DoAtLeastOnce(workId: "NotifyHR", () => NotifyHR(candidateEmail));
+                    return true;
+                },
+                second: r => Task.FromResult(false)
+            );
+
+            if (flowCompleted)
+                return;
+            
+            await es.DoAtLeastOnce(
+                workId: $"Reminder#{i}",
+                SendEngagementReminder
+            );
+        }
+
+        throw new Exception("Max number of retries exceeded");
+    }
+
+    private static Task NotifyHR(string candidateEmail) => Task.CompletedTask;
+    private static Task SendEngagementInitialCorrespondence() => Task.CompletedTask;
+    private static Task SendEngagementReminder() => Task.CompletedTask;
+}
+
+public record EngagementAccepted(int Iteration);
+public record EngagementRejected(int Iteration);
---
+++ Samples/Sample.ConsoleApp/Engagement/Example.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using Cleipnir.ResilientFunctions;
+using Cleipnir.ResilientFunctions.CoreRuntime.Invocation;
+using Cleipnir.ResilientFunctions.Domain;
+using Cleipnir.ResilientFunctions.Helpers;
+using Cleipnir.ResilientFunctions.Storage;
+
+namespace ConsoleApp.Engagement;
+
+public static class Example
+{
+    public static async Task Perform()
+    {
+        var store = new InMemoryFunctionStore();
+        
+        var functions = new RFunctions(
+            store,
+            new Settings(unhandledExceptionHandler: Console.WriteLine)
+        );
+
+        var rAction = functions
+            .RegisterAction(
+             "EngagementSaga",
+             (string email, Context context) => EngagementSaga.Start(email, context)
+            );
+
+        const string customerEmail = "peter@gmail.com";
+        await rAction.Schedule(
+            functionInstanceId: customerEmail,
+            param: customerEmail
+        );
+
+        var eventSourceWriter = rAction.EventSourceWriters.For(customerEmail);
+        await eventSourceWriter.AppendEvent(new EngagementRejected(0));
+        await Task.Delay(3_000);
+        await eventSourceWriter.AppendEvent(new EngagementAccepted(1));
+
+        var controlPanel = await rAction.ControlPanels.For(customerEmail);
+        await BusyWait.Until(async () =>
+            {
+                await controlPanel!.Refresh();
+                return controlPanel.Status == Status.Succeeded;
+            }, maxWait: TimeSpan.FromSeconds(10)
+        );
+
+        Console.WriteLine("EngagementSaga completed successfully");
+    }
+}
