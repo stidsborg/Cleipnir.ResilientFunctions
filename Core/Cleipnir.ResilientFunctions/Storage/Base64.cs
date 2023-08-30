26161d20b953524a884e7dbbc5df6551eab8fdf2
stidsborg <stidsborg@cleipnir.net>
2023-08-30 10:52:38 +0200

    Added Base64 utility class

---
+++ Core/Cleipnir.ResilientFunctions/Storage/Base64.cs
@@ -0,0 +1,16 @@
+﻿namespace Cleipnir.ResilientFunctions.Storage;
+
+public static class Base64
+{
+    public static string Base64Encode(string text) 
+    {
+        var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(text);
+        return System.Convert.ToBase64String(plainTextBytes);
+    }
+    
+    public static string Base64Decode(string base64EncodedData) 
+    {
+        var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+        return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+    }
+}
