5a6587229ef90cd605f22c18977350eec62710de
stidsborg <stidsborg@cleipnir.net>
2024-02-02 12:31:09 +0100

    Added SignupFlow sample

---
+++ Samples/Sample.ConsoleApp/CustomerSignUp/Example.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Cleipnir.ResilientFunctions;
+using Cleipnir.ResilientFunctions.Domain;
+using Cleipnir.ResilientFunctions.Storage;
+
+namespace ConsoleApp.CustomerSignUp;
+
+public static class Example
+{
+    public static async Task Perform()
+    {
+        var store = new InMemoryFunctionStore();
+        
+        var functions = new FunctionsRegistry(
+            store,
+            new Settings(unhandledExceptionHandler: Console.WriteLine)
+        );
+
+        var rAction = functions
+            .RegisterAction<string>(
+             "CustomerSignupFlow",
+                SignupFlow.Start
+            ).Invoke;
+
+        var offerDate = new DateOnly(2022, 1, 1);
+        await rAction(
+            functionInstanceId: offerDate.ToString(),
+            param: "billgates@microsoft.net"
+        );
+        
+        Console.WriteLine("Offers sent successfully");
+    }
+}
---
+++ Samples/Sample.ConsoleApp/CustomerSignUp/SignupFlow.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Cleipnir.ResilientFunctions.CoreRuntime.Invocation;
+using Cleipnir.ResilientFunctions.Reactive.Extensions;
+
+namespace ConsoleApp.CustomerSignUp;
+
+public static class SignupFlow
+{
+    public static async Task Start(string customerEmail, Context context)
+    {
+        var (activities, messages) = context;
+        
+        await activities.Do("SendWelcomeMail", () => SendWelcomeMail(customerEmail));
+
+        for (var i = 0; i <= 5; i++)
+        {
+            var emailVerifiedOption = await messages.FirstOfType<EmailVerified>($"Timeout_{i}", TimeSpan.FromDays(1));
+            if (emailVerifiedOption.HasValue)
+                break;
+            
+            await activities.Do($"Reminder_{i}", () => SendReminderMail(customerEmail));
+            
+            if (i == 5)
+                throw new UserSignupFailedException($"User '{customerEmail}' did not activate within threshold");
+        }
+        
+        await context.Delay("DelayUntilTomorrow", TimeSpan.FromDays(1)); //uses activity and postpone functionality?!
+        await activities.Do("SendWelcomeMail", () => SendFollowUpMail(customerEmail));
+    }
+
+    private static Task SendWelcomeMail(string customerEmail) => Task.CompletedTask;
+    private static Task SendReminderMail(string customerEmail) => Task.CompletedTask;
+    private static Task SendFollowUpMail(string customerEmail) => Task.CompletedTask;
+
+    public class UserSignupFailedException : Exception
+    {
+        public UserSignupFailedException(string? message) : base(message) {}
+    }
+}
+
+public record EmailVerified(string EmailAddress);
