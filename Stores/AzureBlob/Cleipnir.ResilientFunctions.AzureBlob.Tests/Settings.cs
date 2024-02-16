8a25b517f030baa0843f897ca95e8b93e84405e3
stidsborg <stidsborg@cleipnir.net>
2024-02-16 08:14:11 +0100

    Removed AzureBlob-persistence

--- Stores/AzureBlob/Cleipnir.ResilientFunctions.AzureBlob.Tests/Settings.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Cleipnir.ResilientFunctions.AzureBlob.Tests;
-
-public static class Settings
-{
-    public static string? ConnectionString { get; } = Environment.GetEnvironmentVariable("AZURE_BLOB_CONNECTIONSTRING");
-}
