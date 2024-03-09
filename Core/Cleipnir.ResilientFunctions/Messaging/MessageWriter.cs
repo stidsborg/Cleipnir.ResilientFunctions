cfb74045ddb0fef0e84bdd938e4405333d1bcf3a
stidsborg <stidsborg@cleipnir.net>
2024-03-09 08:24:25 +0100

    Removed unneeded using-declaration in MessageWriter

--- Core/Cleipnir.ResilientFunctions/Messaging/MessageWriter.cs
+++ Core/Cleipnir.ResilientFunctions/Messaging/MessageWriter.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Cleipnir.ResilientFunctions.CoreRuntime.Invocation;
 using Cleipnir.ResilientFunctions.CoreRuntime.ParameterSerialization;
 using Cleipnir.ResilientFunctions.Domain;
