dab85ebee0b8006b6710d65087fdf3c6309d9c5c
stidsborg <stidsborg@cleipnir.net>
2024-05-20 10:10:29 +0200

    Minimize sql-script string generations in PostgresStatesStore

--- Stores/PostgreSQL/Cleipnir.ResilientFunctions.PostgreSQL/PostgresStatesStore.cs
+++ Stores/PostgreSQL/Cleipnir.ResilientFunctions.PostgreSQL/PostgresStatesStore.cs
@@ -18,32 +18,35 @@ public class PostgresStatesStore : IStatesStore
         _tablePrefix = tablePrefix;
     }
 
+    private string? _initializeSql;
     public async Task Initialize()
     {
         await using var conn = await CreateConnection();
-        var sql = @$"
+        _initializeSql ??= @$"
             CREATE TABLE IF NOT EXISTS {_tablePrefix}_states (
                 id VARCHAR(450) PRIMARY KEY,
                 state TEXT NOT NULL
             );";
-        var command = new NpgsqlCommand(sql, conn);
+        var command = new NpgsqlCommand(_initializeSql, conn);
         await command.ExecuteNonQueryAsync();
     }
 
+    private string? _truncateSql;
     public async Task Truncate()
     {
         await using var conn = await CreateConnection();
-        var sql = $"TRUNCATE TABLE {_tablePrefix}_states";
-        var command = new NpgsqlCommand(sql, conn);
+        _truncateSql ??= $"TRUNCATE TABLE {_tablePrefix}_states";
+        var command = new NpgsqlCommand(_truncateSql, conn);
         await command.ExecuteNonQueryAsync();
     }
 
+    private string? _upsertStateSql;
     public async Task UpsertState(FunctionId functionId, StoredState storedState)
     {
         var (functionTypeId, functionInstanceId) = functionId;
         
         await using var conn = await CreateConnection();
-        var sql = $@"
+        _upsertStateSql ??= $@"
           INSERT INTO {_tablePrefix}_states 
               (id, state)
           VALUES
@@ -52,7 +55,7 @@ public class PostgresStatesStore : IStatesStore
           DO 
             UPDATE SET state = EXCLUDED.state";
         
-        await using var command = new NpgsqlCommand(sql, conn)
+        await using var command = new NpgsqlCommand(_upsertStateSql, conn)
         {
             Parameters =
             {
@@ -64,14 +67,15 @@ public class PostgresStatesStore : IStatesStore
         await command.ExecuteNonQueryAsync();
     }
 
+    private string? _getStatesSql;
     public async Task<IEnumerable<StoredState>> GetStates(FunctionId functionId)
     {
         await using var conn = await CreateConnection();
-        var sql = @$"
+        _getStatesSql ??= @$"
             SELECT id, state
             FROM {_tablePrefix}_states
             WHERE id LIKE $1";
-        await using var command = new NpgsqlCommand(sql, conn)
+        await using var command = new NpgsqlCommand(_getStatesSql, conn)
         {
             Parameters =
             {
@@ -93,13 +97,14 @@ public class PostgresStatesStore : IStatesStore
         return functions;
     }
 
+    private string? _removeStateSql;
     public async Task RemoveState(FunctionId functionId, StateId stateId)
     {
         await using var conn = await CreateConnection();
-        var sql = $"DELETE FROM {_tablePrefix}_states WHERE id = $1";
+        _removeStateSql ??= $"DELETE FROM {_tablePrefix}_states WHERE id = $1";
         
         var id = Escaper.Escape(functionId.TypeId.Value, functionId.InstanceId.Value, stateId.Value);
-        await using var command = new NpgsqlCommand(sql, conn)
+        await using var command = new NpgsqlCommand(_removeStateSql, conn)
         {
             Parameters = { new() {Value = id } }
         };
@@ -107,13 +112,14 @@ public class PostgresStatesStore : IStatesStore
         await command.ExecuteNonQueryAsync();
     }
 
+    private string? _removeSql;
     public async Task Remove(FunctionId functionId)
     {
         await using var conn = await CreateConnection();
-        var sql = $"DELETE FROM {_tablePrefix}_states WHERE id LIKE $1";
+        _removeSql ??= $"DELETE FROM {_tablePrefix}_states WHERE id LIKE $1";
         
         var idPrefix = Escaper.Escape(functionId.TypeId.Value, functionId.InstanceId.Value) + $"{Escaper.Separator}%";
-        await using var command = new NpgsqlCommand(sql, conn)
+        await using var command = new NpgsqlCommand(_removeSql, conn)
         {
             Parameters = { new() {Value = idPrefix } }
         };
