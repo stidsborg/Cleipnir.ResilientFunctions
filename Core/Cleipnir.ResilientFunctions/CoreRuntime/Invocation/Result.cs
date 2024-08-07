18e7e7a5ba194b499b681e9d419db488bee802c3
stidsborg <stidsborg@cleipnir.net>
2024-08-07 08:49:31 +0200

    Removed unused code

--- Core/Cleipnir.ResilientFunctions/CoreRuntime/Invocation/Result.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Cleipnir.ResilientFunctions.CoreRuntime.Invocation;
-
-public enum PersistResultReturn
-{
-    Success = 0,
-    ScheduleReInvocation = 1,
-    ConcurrentModification = 2,
-}
