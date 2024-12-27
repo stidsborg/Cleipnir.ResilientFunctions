ff694bf099db2b1535b45115a0d7c5c5fc1adfb3
stidsborg <stidsborg@cleipnir.net>
2024-12-27 09:55:30 +0100

    Removed unused WorkRecords.cs

--- Core/Cleipnir.ResilientFunctions/Reactive/Utilities/WorkRecords.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Cleipnir.ResilientFunctions.Reactive.Utilities;
-
-public record WorkStarted(string WorkId);
-public record WorkCompleted(string WorkId);
-public record WorkWithResultCompleted<T>(string WorkId, T Result) : WorkCompleted(WorkId);
