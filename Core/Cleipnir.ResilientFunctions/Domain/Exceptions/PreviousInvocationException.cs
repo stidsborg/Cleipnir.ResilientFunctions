b7ae71ac46291c1d8dec2c98a6ea6c8b1364d7c2
stidsborg <stidsborg@cleipnir.net>
2025-01-12 10:47:41 +0100

    Added previous exception error message to PreviousInvocationException

--- Core/Cleipnir.ResilientFunctions/Domain/Exceptions/PreviousInvocationException.cs
+++ Core/Cleipnir.ResilientFunctions/Domain/Exceptions/PreviousInvocationException.cs
@@ -1,7 +1,7 @@
 ﻿namespace Cleipnir.ResilientFunctions.Domain.Exceptions;
 
 public sealed class PreviousInvocationException(FlowType flowType, PreviouslyThrownException exception)
-    : FlowTypeException(flowType, "Previous invocation failed")
+    : FlowTypeException(flowType, "Previous invocation failed: " + System.Environment.NewLine + exception.ErrorMessage)
 {
     public PreviouslyThrownException Exception { get; } = exception;
 }
