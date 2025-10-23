739621568dfddd7481b560be6e5f728782950e17
stidsborg <stidsborg@cleipnir.net>
2025-10-23 18:40:56 +0200

    Added append-method too BinaryPacker

--- Core/Cleipnir.ResilientFunctions.Tests/UtilsTests/BinaryPackerTests.cs
+++ Core/Cleipnir.ResilientFunctions.Tests/UtilsTests/BinaryPackerTests.cs
@@ -63,4 +63,43 @@ public class BinaryPackerTests
         split[1].ShouldBeNull();
         split[2].ShouldBe(last);
     }
+    
+    [TestMethod]
+    public void MultipleStringsCanBeAppendedToSourceAndAllReconstructed()
+    {
+        const string str1 = "hello world";
+        const string str2 = "hello universe";
+        
+        var sourceArray = BinaryPacker.Pack(str1.ToUtf8Bytes(), str2.ToUtf8Bytes());        
+        
+        const string str3 = "testing tester";
+        const string str4 = "";
+        const string str5 = "|";
+
+        var appended = BinaryPacker.Append(sourceArray, str3.ToUtf8Bytes(), str4.ToUtf8Bytes(), str5.ToUtf8Bytes());
+        
+        var reconstructed = BinaryPacker.Split(appended, pieces: 5);
+        reconstructed.Count.ShouldBe(5);
+        reconstructed[0]!.ToStringFromUtf8Bytes().ShouldBe(str1);
+        reconstructed[1]!.ToStringFromUtf8Bytes().ShouldBe(str2);
+        reconstructed[2]!.ToStringFromUtf8Bytes().ShouldBe(str3);
+        reconstructed[3]!.ToStringFromUtf8Bytes().ShouldBe(str4);
+        reconstructed[4]!.ToStringFromUtf8Bytes().ShouldBe(str5);
+    }
+    
+    [TestMethod]
+    public void NullCanBeAppendedToSourceAndAllReconstructed()
+    {
+        const string str1 = "hello world";
+        const string str2 = "hello universe";
+        
+        var sourceArray = BinaryPacker.Pack(str1.ToUtf8Bytes(), str2.ToUtf8Bytes());        
+        var appended = BinaryPacker.Append(sourceArray, [null]);
+        
+        var reconstructed = BinaryPacker.Split(appended, pieces: 5);
+        reconstructed.Count.ShouldBe(3);
+        reconstructed[0]!.ToStringFromUtf8Bytes().ShouldBe(str1);
+        reconstructed[1]!.ToStringFromUtf8Bytes().ShouldBe(str2);
+        reconstructed[2].ShouldBeNull();
+    }
 }
--- Core/Cleipnir.ResilientFunctions/Storage/Utils/BinaryPacker.cs
+++ Core/Cleipnir.ResilientFunctions/Storage/Utils/BinaryPacker.cs
@@ -56,4 +56,33 @@ public static class BinaryPacker
         
         return arrays.ToArray();
     }
+    
+    public static byte[] Append(byte[] source, params byte[]?[] arrays)
+    {
+        var combinedArrSize = arrays.Sum(arr => arr?.Length ?? 0) + sizeof(int) * arrays.Length;
+        var combinedArr = new byte[source.Length + combinedArrSize];
+        
+        Array.Copy(source, combinedArr, source.Length);
+        
+        var outer = source.Length;
+        foreach(var array in arrays)
+        {
+            var length = array?.Length ?? -1;
+            var lengthBytes = BitConverter.GetBytes(IPAddress.HostToNetworkOrder(length));
+            combinedArr[outer] = lengthBytes[0];
+            combinedArr[++outer] = lengthBytes[1]; 
+            combinedArr[++outer] = lengthBytes[2]; 
+            combinedArr[++outer] = lengthBytes[3];
+            outer++;
+            
+            if (array == null)
+                continue;
+            
+            Array.Copy(array, sourceIndex: 0, combinedArr, destinationIndex: outer, length: array.Length);
+            outer += array.Length;
+        }
+
+        return combinedArr;
+    }
+
 }
