9dc70c9804cb1fcfa80fcd3203666da16c977292
stidsborg <stidsborg@cleipnir.net>
2025-12-15 09:08:09 +0100

    wip

---
+++ Core/Cleipnir.ResilientFunctions/Queuing/IdempotencyKeys.cs
@@ -0,0 +1,12 @@
+using System.Threading.Tasks;
+using Cleipnir.ResilientFunctions.Domain;
+
+namespace Cleipnir.ResilientFunctions.Queuing;
+
+internal class IdempotencyKeys(EffectId id)
+{
+    public async Task Initialize()
+    {
+        
+    }
+}
