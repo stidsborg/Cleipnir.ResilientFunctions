f3a14118e2afd58afbbf743d3e4396f87de944b5
stidsborg <stidsborg@cleipnir.net>
2026-01-21 16:27:50 +0100

    wip

--- Core/Cleipnir.ResilientFunctions.Tests/TestTemplates/FunctionTests/SunshineTests.cs
+++ Core/Cleipnir.ResilientFunctions.Tests/TestTemplates/FunctionTests/SunshineTests.cs
@@ -348,7 +348,7 @@ public abstract class SunshineTests
                 async (string s, Workflow workflow) =>
                 {
                     initialEffectValue = workflow.Effect.Get<string>("InitialEffectId");
-                    initialMessageValue = await workflow.Messages.OfType<string>().First();
+                    initialMessageValue = await workflow.Message<string>();
                     return s;
                 });
 
