25ae1259a4ad3b26cfa789a635df054a36ca6ee7
stidsborg <stidsborg@cleipnir.net>
2026-01-25 09:43:59 +0100

    Removed more Reactive related types

--- Core/Cleipnir.ResilientFunctions/Reactive/Utilities/NoResultException.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Cleipnir.ResilientFunctions.Reactive.Utilities
-{
-    public class NoResultException : Exception
-    {
-        public NoResultException() : base(message: "No event was emitted before the stream completed") { }
-    }
-}
--- Core/Cleipnir.ResilientFunctions/Reactive/Utilities/StreamCompletedException.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Cleipnir.ResilientFunctions.Reactive.Utilities
-{
-    public class StreamCompletedException : Exception
-    {
-        public StreamCompletedException() : base("Cannot subscribe to already completed stream") { }
-    }
-}
