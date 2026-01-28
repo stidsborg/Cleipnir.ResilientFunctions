e67e0529f902ee84ebe2a5eb0327433975aee88d
stidsborg <stidsborg@cleipnir.net>
2026-01-28 09:26:34 +0100

    Removed FunctionCompletion

--- Core/Cleipnir.ResilientFunctions/Messaging/FunctionCompletion.cs
+++
@@ -1,6 +0,0 @@
-using Cleipnir.ResilientFunctions.Domain;
-
-namespace Cleipnir.ResilientFunctions.Messaging;
-
-public record FunctionCompletion<TResult>(TResult Result, FlowId Sender) : FunctionCompletion(Sender);
-public record FunctionCompletion(FlowId Sender);
