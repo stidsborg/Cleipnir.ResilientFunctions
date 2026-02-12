c7e592a7efb7a99f881eda326de0bf89f6b6883b
stidsborg <stidsborg@cleipnir.net>
2026-02-12 08:47:34 +0100

    Removed unused MessageProcessingException

--- Core/Cleipnir.ResilientFunctions/Messaging/MessageProcessingException.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Cleipnir.ResilientFunctions.Messaging;
-
-public class MessageProcessingException : Exception
-{
-    public MessageProcessingException(Exception innerException) 
-        : base("Processing of events failed", innerException) { }
-}
