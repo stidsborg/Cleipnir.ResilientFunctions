cb24445717df6928f9900b90eb42ace2cd68e90b
stidsborg <stidsborg@cleipnir.net>
2026-05-30 10:52:08 +0200

    DeserializationException simplification

--- Core/Cleipnir.ResilientFunctions/CoreRuntime/Serialization/DeserializationException.cs
+++ Core/Cleipnir.ResilientFunctions/CoreRuntime/Serialization/DeserializationException.cs
@@ -2,8 +2,4 @@
 
 namespace Cleipnir.ResilientFunctions.CoreRuntime.Serialization;
 
-public class DeserializationException : Exception
-{
-    public DeserializationException(string? message, Exception? innerException) 
-        : base(message, innerException) { }
-}
+public class DeserializationException(string? message, Exception? innerException) : Exception(message, innerException);
