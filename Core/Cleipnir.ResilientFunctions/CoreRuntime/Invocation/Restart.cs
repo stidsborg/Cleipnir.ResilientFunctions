bcf0e0aa92c3962e736e6f25257fe47d9a54c88c
stidsborg <stidsborg@cleipnir.net>
2026-05-30 10:49:33 +0200

    Removed Restart delegate

--- Core/Cleipnir.ResilientFunctions/CoreRuntime/Invocation/Restart.cs
+++
@@ -1,6 +0,0 @@
-using System.Threading.Tasks;
-using Cleipnir.ResilientFunctions.Storage;
-
-namespace Cleipnir.ResilientFunctions.CoreRuntime.Invocation;
-
-public delegate Task Restart(StoredId instanceId);
