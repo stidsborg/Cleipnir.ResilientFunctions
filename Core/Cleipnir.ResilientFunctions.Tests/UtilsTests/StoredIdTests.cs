44b7312ff4554261f8df0b02d1f1c7093d97deac
stidsborg <stidsborg@cleipnir.net>
2026-05-31 09:00:49 +0200

    Removed StoredIdFactory

--- Core/Cleipnir.ResilientFunctions.Tests/UtilsTests/StoredIdTests.cs
+++ Core/Cleipnir.ResilientFunctions.Tests/UtilsTests/StoredIdTests.cs
@@ -23,60 +23,6 @@ public class StoredIdTests
     }
     
     [TestMethod]
-    public void DifferentStringsHashToDifferentGuids()
-    {
-        var hash1 = StoredIdFactory.FromString("Test123");
-        var hash2 = StoredIdFactory.FromString("Test124");
-        
-        hash1.ShouldNotBe(hash2);
-    }
-    
-    [TestMethod]
-    public void EqualStringsHashToSameGuids()
-    {
-        var hash1 = StoredIdFactory.FromString("Test123");
-        var hash2 = StoredIdFactory.FromString("Test123");
-        
-        hash1.ShouldBe(hash2);
-    }
-    
-    [TestMethod]
-    public void DifferentIntsHashToDifferentGuids()
-    {
-        var hash1 = StoredIdFactory.FromInt(1);
-        var hash2 = StoredIdFactory.FromInt(2);
-        
-        hash1.ShouldNotBe(hash2);
-    }
-    
-    [TestMethod]
-    public void EqualIntsHashToSameGuids()
-    {
-        var hash1 = StoredIdFactory.FromInt(1);
-        var hash2 = StoredIdFactory.FromInt(1);
-        
-        hash1.ShouldBe(hash2);
-    }
-    
-    [TestMethod]
-    public void DifferentLongsHashToDifferentGuids()
-    {
-        var hash1 = StoredIdFactory.FromLong(1L);
-        var hash2 = StoredIdFactory.FromLong(2L);
-        
-        hash1.ShouldNotBe(hash2);
-    }
-    
-    [TestMethod]
-    public void EqualLongsHashToSameGuids()
-    {
-        var hash1 = StoredIdFactory.FromLong(1L);
-        var hash2 = StoredIdFactory.FromLong(1L);
-        
-        hash1.ShouldBe(hash2);
-    }
-    
-    [TestMethod]
     public void TypeCanBeExtractedFromId()
     {
         var id = StoredId.Create(new StoredType(1), "SomeInstanceId");
